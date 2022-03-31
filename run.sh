5f6d8205fe6ebeb0dfd282f6f0e40fc8b4310205
szotp <qwertyszot@gmail.com>
2022-03-31 19:47:13 +0200

    use binary release

--- run.sh
+++ run.sh
@@ -1,8 +1,12 @@
 # usage: /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/szotp/CoverageTracker/master/run.sh)"
 
-git -C CoverageTracker pull || git clone git@github.com:szotp/CoverageTracker.git
-cd CoverageTracker
-swift build -c release
-cd ..
-COVERAGE=$(./CoverageTracker/.build/release/CoverageTracker)
+curl -L https://github.com/szotp/CoverageTracker/releases/download/latest/CoverageTracker > CoverageTracker
+chmod +x CoverageTracker
+COVERAGE=$(./CoverageTracker)
+
+# git -C CoverageTracker pull || git clone git@github.com:szotp/CoverageTracker.git
+# cd CoverageTracker
+# swift build -c release
+# cd ..
+# COVERAGE=$(./CoverageTracker/.build/release/CoverageTracker)
 envman add --key "COVERAGE" --value "$COVERAGE"
