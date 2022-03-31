93a7346e03d97486da706f3c14fb621e580e3e0e
szotp <qwertyszot@gmail.com>
2022-03-31 19:55:03 +0200

    fix download link

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 # usage: /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/szotp/CoverageTracker/master/run.sh)"
 
-curl -L https://github.com/szotp/CoverageTracker/releases/download/latest/CoverageTracker > CoverageTracker
+curl -L https://github.com/szotp/CoverageTracker/releases/latest/download/CoverageTracker > CoverageTracker
 chmod +x CoverageTracker
 COVERAGE=$(./CoverageTracker)
 
