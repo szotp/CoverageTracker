8e2255dbaab1152ff47daff9d717168c35da0642
szotp <qwertyszot@gmail.com>
2022-03-31 21:07:32 +0200

    improve release script

--- Scripts/release.sh
+++ Scripts/release.sh
@@ -1,4 +1,11 @@
 # example: ./Scripts/release.sh 0.2
 set -e
+
+if [ $(git tag -l "$1") ]; then
+    gh release delete 0.1
+    git tag -d $1
+    git push --delete origin $1
+fi
+
 swift build -c release
 gh release create $1 .build/release/CoverageTracker --generate-notes -n "" -t ""
